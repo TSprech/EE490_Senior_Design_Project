42e6f576b70794dc403b9faab073155d1d69a553
TSprech <trey.sprecher@gmail.com>
2023-04-09 17:30:38 -0400

    Fixed Deprecated Warning
    React was warning that calling render directly was deprecated so that was updated to the React 18 version of doing it.

--- Application/src/app.tsx
+++ Application/src/app.tsx
@@ -1,7 +1,9 @@
-import * as ReactDOM from 'react-dom';
+// import * as ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  const root = createRoot(document.getElementById("root") as HTMLElement); // Thanks: https://stackoverflow.com/questions/71668256/deprecation-notice-reactdom-render-is-no-longer-supported-in-react-18
+  root.render(<h2>Hello from React!</h2>);
 }
 
 render();
