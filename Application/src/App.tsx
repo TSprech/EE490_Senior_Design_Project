5de0b37daf470a1f4b5d2d58c6c100edba7fbdf4
TSprech <trey.sprecher@gmail.com>
2023-04-30 14:23:03 -0400

    Multi Card with Charts
    Created a layout with 6 dynamically resizing cards with charts in them. The height of each chart area is still a bit temperamental but hopefully that can be resolved.

--- Application/src/App.tsx
+++ Application/src/App.tsx
@@ -26,7 +26,7 @@ import Layout from './components/Layout';
 // import useStateObj from './useStateObj';
 import AppBar from './components/AppBar';
 import {JSON_Data_RX} from "./Atoms";
-import {AspectRatio, Button, Card, Divider, Stack, styled} from "@mui/joy";
+import {AspectRatio, Button, Card, Divider, Grid, Stack, styled} from "@mui/joy";
 import Typography from "@mui/joy/Typography";
 import {BookmarkAdd} from "@mui/icons-material";
 import Box from "@mui/joy/Box";
@@ -155,6 +155,54 @@ class App extends Component {
     }
 }
 
+function BasicCard() {
+    return (
+        // <Card variant="outlined" sx={{ width: 320 }}>
+        <Card variant="outlined">
+            <Typography level="h2" fontSize="md" sx={{ mb: 0.5 }}>
+                Yosemite National Park
+            </Typography>
+            <Typography level="body2">April 24 to May 02, 2021</Typography>
+            <IconButton
+                aria-label="bookmark Bahamas Islands"
+                variant="plain"
+                color="neutral"
+                size="sm"
+                sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+            >
+                <BookmarkAdd />
+            </IconButton>
+            {/*<AspectRatio minHeight="200px" maxHeight="200px" sx={{ my: 2 }}>*/}
+            <AspectRatio ratio={'1618 / 1000'} sx={{ my: 2 }}>
+                <App/>
+                {/*<img*/}
+                {/*    src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"*/}
+                {/*    srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"*/}
+                {/*    loading="lazy"*/}
+                {/*    alt=""*/}
+                {/*/>*/}
+            </AspectRatio>
+            <Box sx={{ display: 'flex' }}>
+                <div>
+                    <Typography level="body3">Total price:</Typography>
+                    <Typography fontSize="lg" fontWeight="lg">
+                        $2,900
+                    </Typography>
+                </div>
+                <Button
+                    variant="solid"
+                    size="sm"
+                    color="primary"
+                    aria-label="Explore Bahamas Islands"
+                    sx={{ ml: 'auto', fontWeight: 600 }}
+                >
+                    Explore
+                </Button>
+            </Box>
+        </Card>
+    );
+}
+
 export default function RenderIndex() {
     const [drawerOpen, setDrawerOpen] = React.useState(false);
     // const [ports, setPorts] = React.useState([]); // Manages the list of available ports
@@ -182,38 +230,51 @@ export default function RenderIndex() {
                     <AppBar/>
                     <Layout.Main>
                         <JSONPrint/>
-                        {/*<Card variant="outlined" sx={{width: 420}}>*/}
-                        <Card variant="outlined" sx={{width: 420}}>
-                            <Typography level="h2" fontSize="md" sx={{mb: 0.5}}>
-                                Voltage In (V)
-                            </Typography>
-                            <AspectRatio minHeight="15rem" maxHeight="16rem" sx={{my: 2}}>
-                                {/*<ApexChart/>*/}
-                                <App/>
-                            </AspectRatio>
-                            {/*<Box sx={{display: 'flex'}} justifyContent={"center"}>*/}
-                                <Stack direction={"row"} spacing={{ xs: 1, sm: 2 }} justifyContent="space-evenly" divider={<Divider orientation="vertical"/>}>
-                                    <Item>
-                                        <Typography level="body3">Current: </Typography>
-                                        <Typography fontSize="lg" fontWeight="lg">
-                                            $2,900
-                                        </Typography>
-                                    </Item>
-                                    <Item>
-                                        <Typography level="body3">Local Min: </Typography>
-                                        <Typography fontSize="lg" fontWeight="lg">
-                                            $2,900
-                                        </Typography>
-                                    </Item>
-                                <Item>
-                                    <Typography level="body3">Local Max: </Typography>
-                                    <Typography fontSize="lg" fontWeight="lg">
-                                        $2,900
-                                    </Typography>
-                                </Item>
-                        </Stack>
-                    {/*</Box>*/}
-                </Card>
+                        <Grid
+                            container
+                            spacing={{ xs: 2, md: 3 }}
+                            columns={{ xs: 2, sm: 8, md: 12 }}
+                            sx={{ flexGrow: 1 }}
+                        >
+                            {Array.from(Array(6)).map((_, index) => (
+                                <Grid xs={2} sm={4} md={4} key={index}>
+                                    {/*<Item>xs=2</Item>*/}
+                                    <BasicCard/>
+                                </Grid>
+                            ))}
+                        </Grid>
+                {/*        /!*<Card variant="outlined" sx={{width: 420}}>*!/*/}
+                {/*        <Card variant="outlined" sx={{width: 420}}>*/}
+                {/*            <Typography level="h2" fontSize="md" sx={{mb: 0.5}}>*/}
+                {/*                Voltage In (V)*/}
+                {/*            </Typography>*/}
+                {/*            <AspectRatio minHeight="15rem" maxHeight="16rem" sx={{my: 2}}>*/}
+                {/*                /!*<ApexChart/>*!/*/}
+                {/*                <App/>*/}
+                {/*            </AspectRatio>*/}
+                {/*            /!*<Box sx={{display: 'flex'}} justifyContent={"center"}>*!/*/}
+                {/*                <Stack direction={"row"} spacing={{ xs: 1, sm: 2 }} justifyContent="space-evenly" divider={<Divider orientation="vertical"/>}>*/}
+                {/*                    <Item>*/}
+                {/*                        <Typography level="body3">Current: </Typography>*/}
+                {/*                        <Typography fontSize="lg" fontWeight="lg">*/}
+                {/*                            $2,900*/}
+                {/*                        </Typography>*/}
+                {/*                    </Item>*/}
+                {/*                    <Item>*/}
+                {/*                        <Typography level="body3">Local Min: </Typography>*/}
+                {/*                        <Typography fontSize="lg" fontWeight="lg">*/}
+                {/*                            $2,900*/}
+                {/*                        </Typography>*/}
+                {/*                    </Item>*/}
+                {/*                <Item>*/}
+                {/*                    <Typography level="body3">Local Max: </Typography>*/}
+                {/*                    <Typography fontSize="lg" fontWeight="lg">*/}
+                {/*                        $2,900*/}
+                {/*                    </Typography>*/}
+                {/*                </Item>*/}
+                {/*        </Stack>*/}
+                {/*    /!*</Box>*!/*/}
+                {/*</Card>*/}
             </Layout.Main>
         </Layout.Root>
 </CssVarsProvider>
