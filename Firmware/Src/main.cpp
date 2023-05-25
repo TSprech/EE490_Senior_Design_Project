d4cb94bacd17b1ad1d91501b04b14b8f303b9bed
TSprech <trey.sprecher+github@gmail.com>
2023-05-25 16:02:38 -0400

    Initial DMA Abstraction
    Created the C++ abstraction layer for the DMA, but currently it does not support the IQR functions.

--- Firmware/Src/main.cpp
+++ Firmware/Src/main.cpp
@@ -1,12 +1,12 @@
-#include "nlohmann/json.hpp"
-#include "hardware/gpio.h"
-#include "GetLine.hpp"
-#include "pico/stdio.h"
-#include "pico/time.h"
-#include "hardware/adc.h"
 #include <iostream>
 
+#include "GetLine.hpp"
 #include "ModuloScheduler.hpp"
+#include "hardware/adc.h"
+#include "hardware/gpio.h"
+#include "nlohmann/json.hpp"
+#include "pico/stdio.h"
+#include "pico/time.h"
 
 using json = nlohmann::json;
 
@@ -14,19 +14,19 @@ using namespace units::literals;
 
 #include <pico/time.h>
 
+#include "DMA.hpp"
 #include "FastPID.hpp"
+#include "GPIO.hpp"
 #include "RP2040_PWM.hpp"
 #include "TypedUnits.hpp"
+#include "hardware/i2c.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
-#include "hardware/i2c.h"
-
-#include "GPIO.hpp"
 using namespace rpp;
 
 auto pwm_manager = pwm::PWMManager<0, 1>(true);
 
-constexpr float Kp=2, Ki=0.5, Kd=0, Hz=10;
+constexpr float Kp = 2, Ki = 0.5, Kd = 0, Hz = 10;
 constexpr bool output_signed = false;
 
 constinit pid::FastPID myPID;
@@ -34,15 +34,19 @@ constinit pid::FastPID myPID;
 int main() {
   stdio_init_all();
 
+  int32_t destination = 0;
+  auto channel = dma::DMA();
+  if (channel.Claim()) channel.Init().Trigger(dma::Triggers::pio_0_rx_0).TransferSize(dma::TransferSizes::s_32_bit).WriteAddress(&destination).HighPriority(dma::States::enabled).Enable(dma::States::enabled);
+
   gpio::gpio_0.Init().Direction(gpio::Directions::output).Pull(gpio::Pulls::disable).SlewRate(gpio::SlewRates::fast).DriveStrength(gpio::DriveStrengths::ds_12mA).Write(gpio::Levels::low);
   if (!myPID.Initialize(Kp, Ki, Kd, Hz, output_signed)) {
     puts("PID initialization error");
   }
 
-//  auto quant_1 = (units::frequency::hertz_i32_t{82} + static_cast<units::frequency::hertz_i32_t>(units::frequency::hertz_u32_t{32})).value();
-//  auto quant_2 = (units::frequency::hertz_i32_t{82} + units::frequency::hertz_i32_t{32}).value();
-//  //    auto quant_2 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{82}).value();
-//  //    auto quant_3 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{2}).value();
+  //  auto quant_1 = (units::frequency::hertz_i32_t{82} + static_cast<units::frequency::hertz_i32_t>(units::frequency::hertz_u32_t{32})).value();
+  //  auto quant_2 = (units::frequency::hertz_i32_t{82} + units::frequency::hertz_i32_t{32}).value();
+  //  //    auto quant_2 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{82}).value();
+  //  //    auto quant_3 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{2}).value();
 
   constexpr auto led_pin = PICO_DEFAULT_LED_PIN;
   gpio_init(led_pin);
@@ -53,61 +57,62 @@ int main() {
   gpio_put(led_pin, false);
   sleep_ms(250);
 
-//  std::array<char, 512> buf{};
-//  json j;
-    printf("ADC Example, measuring GPIO26\n");
-    sleep_ms(3000);
-//    printf("Outmin: %lli, Outmax: %lli, DutyMin: %lu, DutyMax: %lu\n", myPID.outmin_, myPID.outmax_, 0, pwm::PWMManager<0, 1>::MaxDuty());
-      adc_init();
-      // Make sure GPIO is high-impedance, no pullups etc
-      adc_gpio_init(26);
-      // Select ADC input 0 (GPIO26)
-      adc_select_input(0);
+  //  std::array<char, 512> buf{};
+  //  json j;
+  printf("ADC Example, measuring GPIO26\n");
+  sleep_ms(3000);
+  //    printf("Outmin: %lli, Outmax: %lli, DutyMin: %lu, DutyMax: %lu\n", myPID.outmin_, myPID.outmax_, 0, pwm::PWMManager<0, 1>::MaxDuty());
+  adc_init();
+  // Make sure GPIO is high-impedance, no pullups etc
+  adc_gpio_init(26);
+  // Select ADC input 0 (GPIO26)
+  adc_select_input(0);
 
   srand(64);
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
-      pwm_manager.Initialize();
-      pwm_manager.DeadBand(1);
-      pwm_manager.Frequency(100'000);
-      pwm_manager.Enable();
+  pwm_manager.Initialize();
+  pwm_manager.DeadBand(1);
+  pwm_manager.Frequency(100'000);
+  pwm_manager.Enable();
 
-      const float conversion_factor = 3.3f / (1 << 12);
+  const float conversion_factor = 3.3f / (1 << 12);
   while (true) {
-      uint32_t sum = 0;
-      for (int i = 0; i < 100; ++i) {
-        sum += adc_read();
-      }
-      auto result = sum / 100;
-      int16_t millivoltage = static_cast<float>(result) * conversion_factor * 1000.0;
-      auto start = get_absolute_time();
-      uint16_t output = myPID.Evaluate(2486, millivoltage);
-//      auto diff = get_absolute_time()._private_us_since_boot - start._private_us_since_boot;
-      auto diff = 0;
-      auto duty = Map<int64_t>(output, 0, UINT16_MAX, 0, pwm::PWMManager<0, 1>::MaxDuty());
-      pwm_manager.DutyCycle(duty);
-      std::cout << "Output: " << output << " Duty: " << duty << " Voltage: " << millivoltage << "mV" << "Time: " << diff << "us\n";
-//      printf("Output: %u, Duty: %lu, Raw value: 0x%04lx, voltage: %d mV\n", output, duty, result, millivoltage);
-      sleep_ms(98);
+    uint32_t sum = 0;
+    for (int i = 0; i < 100; ++i) {
+      sum += adc_read();
+    }
+    auto result = sum / 100;
+    int16_t millivoltage = static_cast<float>(result) * conversion_factor * 1000.0;
+    auto start = get_absolute_time();
+    uint16_t output = myPID.Evaluate(2486, millivoltage);
+    //      auto diff = get_absolute_time()._private_us_since_boot - start._private_us_since_boot;
+    auto diff = 0;
+    auto duty = Map<int64_t>(output, 0, UINT16_MAX, 0, pwm::PWMManager<0, 1>::MaxDuty());
+    pwm_manager.DutyCycle(duty);
+    std::cout << "Output: " << output << " Duty: " << duty << " Voltage: " << millivoltage << "mV"
+              << "Time: " << diff << "us\n";
+    //      printf("Output: %u, Duty: %lu, Raw value: 0x%04lx, voltage: %d mV\n", output, duty, result, millivoltage);
+    sleep_ms(98);
 
-//    auto in_str = GetLine(buf);                   // Read in some JSON if it is available
-//    if (!in_str.empty()) {                        // If it is available, it will be in the string_view that sits within buf
-//      if (json::accept(in_str)) {                 // Check if JSON is valid (if it isn't an exception is raised and core 0 crashes)
-//        j = json::parse(in_str, nullptr, false);  // Parse the JSON in
-//        if (j.contains("LED")) {                  // Check if LED is sent
-//          gpio_put(led_pin, j["LED"]);
-//        }
-//      }
-//    }
-//    sleep_ms(1000);
-//    j.clear();
-//    j["Int"] = 24;
-//    j["String"] = "Hello World!";
-//    j["Float"] = 3.1415;
-//    j["Bool"] = true;
-//    auto str = j.dump();
-//    puts(str.data());
+    //    auto in_str = GetLine(buf);                   // Read in some JSON if it is available
+    //    if (!in_str.empty()) {                        // If it is available, it will be in the string_view that sits within buf
+    //      if (json::accept(in_str)) {                 // Check if JSON is valid (if it isn't an exception is raised and core 0 crashes)
+    //        j = json::parse(in_str, nullptr, false);  // Parse the JSON in
+    //        if (j.contains("LED")) {                  // Check if LED is sent
+    //          gpio_put(led_pin, j["LED"]);
+    //        }
+    //      }
+    //    }
+    //    sleep_ms(1000);
+    //    j.clear();
+    //    j["Int"] = 24;
+    //    j["String"] = "Hello World!";
+    //    j["Float"] = 3.1415;
+    //    j["Bool"] = true;
+    //    auto str = j.dump();
+    //    puts(str.data());
   }
 #pragma clang diagnostic pop
 }
