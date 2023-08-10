e54c5bf365b1ca08e9e723754436b180cef8c5f2
TSprech <trey.sprecher+github@gmail.com>
2023-08-10 17:32:07 -0400

    Implemented Incremental Conductance Algorithm
    Based on a Microchip white paper, an incremental conductance algorithm was implemented. It does not directly change the duty cycle, but instead returns an amount for the duty cycle to change.

--- Firmware/Src/main.cpp
+++ Firmware/Src/main.cpp
@@ -1,147 +1,3 @@
-//#include <iostream>
-//
-//#include "GetLine.hpp"
-//#include "ModuloScheduler.hpp"
-//#include "hardware/adc.h"
-//#include "hardware/gpio.h"
-//#include "nlohmann/json.hpp"
-//#include "pico/stdio.h"
-//#include "pico/time.h"
-//
-//using json = nlohmann::json;
-//
-//using namespace units::literals;
-//
-//#include <pico/time.h>
-//
-//#include "DMA.hpp"
-//#include "FastPID.hpp"
-//#include "GPIO.hpp"
-//#include "RP2040_PWM.hpp"
-//#include "TypedUnits.hpp"
-//#include "hardware/i2c.h"
-//#include "pico/stdlib.h"
-//#include "pico/time.h"
-//using namespace rpp;
-//
-//auto pwm_manager = pwm::PWMManager<0, 1>(true);
-//
-//constexpr float Kp = 2, Ki = 0.5, Kd = 0, Hz = 10;
-//constexpr bool output_signed = false;
-//
-//constinit pid::FastPID myPID;
-//
-//int main() {
-//  stdio_init_all();
-//
-//  int32_t destination = 0;
-//  auto channel = dma::DMA();
-//  if (channel.Claim()) channel.Init().Trigger(dma::Triggers::pio_0_rx_0).TransferSize(dma::TransferSizes::s_32_bit).WriteAddress(&destination).HighPriority(dma::States::enabled).Enable(dma::States::enabled);
-//
-//  gpio::gpio_0.Init().Direction(gpio::Directions::output).Pull(gpio::Pulls::disable).SlewRate(gpio::SlewRates::fast).DriveStrength(gpio::DriveStrengths::ds_12mA).Write(gpio::Levels::low);
-//  if (!myPID.Initialize(Kp, Ki, Kd, Hz, output_signed)) {
-//    puts("PID initialization error");
-//  }
-//
-//  //  auto quant_1 = (units::frequency::hertz_i32_t{82} + static_cast<units::frequency::hertz_i32_t>(units::frequency::hertz_u32_t{32})).value();
-//  //  auto quant_2 = (units::frequency::hertz_i32_t{82} + units::frequency::hertz_i32_t{32}).value();
-//  //  //    auto quant_2 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{82}).value();
-//  //  //    auto quant_3 = (units::frequency::hertz_i32_t{32} + units::frequency::kilohertz_u32_t{2}).value();
-//
-//  constexpr auto led_pin = PICO_DEFAULT_LED_PIN;
-//  gpio_init(led_pin);
-//  gpio_set_dir(led_pin, GPIO_OUT);
-//
-//  gpio_put(led_pin, true);
-//  sleep_ms(250);
-//  gpio_put(led_pin, false);
-//  sleep_ms(250);
-//
-//  //  std::array<char, 512> buf{};
-//  //  json j;
-//  printf("ADC Example, measuring GPIO26\n");
-//  sleep_ms(3000);
-//  //    printf("Outmin: %lli, Outmax: %lli, DutyMin: %lu, DutyMax: %lu\n", myPID.outmin_, myPID.outmax_, 0, pwm::PWMManager<0, 1>::MaxDuty());
-//  adc_init();
-//  // Make sure GPIO is high-impedance, no pullups etc
-//  adc_gpio_init(26);
-//  // Select ADC input 0 (GPIO26)
-//  adc_select_input(0);
-//
-//  srand(64);
-//
-//#pragma clang diagnostic push
-//#pragma ide diagnostic ignored "EndlessLoop"
-//  pwm_manager.Initialize();
-//  pwm_manager.DeadBand(1);
-//  pwm_manager.Frequency(100'000);
-//  pwm_manager.Enable();
-//
-//  const float conversion_factor = 3.3f / (1 << 12);
-//  while (true) {
-//    uint32_t sum = 0;
-//    for (int i = 0; i < 100; ++i) {
-//      sum += adc_read();
-//    }
-//    auto result = sum / 100;
-//    int16_t millivoltage = static_cast<float>(result) * conversion_factor * 1000.0;
-//    auto start = get_absolute_time();
-//    uint16_t output = myPID.Evaluate(2486, millivoltage);
-//    //      auto diff = get_absolute_time()._private_us_since_boot - start._private_us_since_boot;
-//    auto diff = 0;
-//    auto duty = Map<int64_t>(output, 0, UINT16_MAX, 0, pwm::PWMManager<0, 1>::MaxDuty());
-//    pwm_manager.DutyCycle(duty);
-//    std::cout << "Output: " << output << " Duty: " << duty << " Voltage: " << millivoltage << "mV"
-//              << "Time: " << diff << "us\n";
-//    //      printf("Output: %u, Duty: %lu, Raw value: 0x%04lx, voltage: %d mV\n", output, duty, result, millivoltage);
-//    sleep_ms(98);
-//
-//    //    auto in_str = GetLine(buf);                   // Read in some JSON if it is available
-//    //    if (!in_str.empty()) {                        // If it is available, it will be in the string_view that sits within buf
-//    //      if (json::accept(in_str)) {                 // Check if JSON is valid (if it isn't an exception is raised and core 0 crashes)
-//    //        j = json::parse(in_str, nullptr, false);  // Parse the JSON in
-//    //        if (j.contains("LED")) {                  // Check if LED is sent
-//    //          gpio_put(led_pin, j["LED"]);
-//    //        }
-//    //      }
-//    //    }
-//    //    sleep_ms(1000);
-//    //    j.clear();
-//    //    j["Int"] = 24;
-//    //    j["String"] = "Hello World!";
-//    //    j["Float"] = 3.1415;
-//    //    j["Bool"] = true;
-//    //    auto str = j.dump();
-//    //    puts(str.data());
-//  }
-//#pragma clang diagnostic pop
-//}
-////  stdio_init_all();
-////  pwm_manager.Initialize();
-////  pwm_manager.DeadBand(1);
-////
-////  while (true) {
-////    pwm_manager.Enable();
-////    pwm_manager.Frequency(100'000);
-////    for (int i = 0; i < 1'000'000; i += 5'000) {
-////      pwm_manager.DutyCycle(i);  // 10%
-////      sleep_us(10);
-////    }
-////    pwm_manager.Disable();
-////    sleep_ms(1);
-////    pwm_manager.Enable();
-////    pwm_manager.Frequency(500'000);
-////    for (int i = 0; i < 1'000'000; i += 5'000) {
-////      pwm_manager.DutyCycle(i);  // 10%
-////      sleep_us(10);
-////    }
-////    pwm_manager.Disable();
-////    sleep_ms(10);
-////  }
-////
-////  return 0;
-////}
-
 #include <algorithm>
 #include <charconv>
 
@@ -152,10 +8,73 @@
 #include "fmt/format.h"
 #include "pico/stdlib.h"
 
+#include "TypedUnits.hpp"
+
+template<typename T>
+class DeltaVariable {
+public:
+  constexpr explicit DeltaVariable(T initial_value) noexcept: value_(initial_value), previous_value_(initial_value) {}
+
+  constexpr auto operator=(T value) noexcept -> DeltaVariable & {
+    this->previous_value_ = value_;
+    this->value_ = value;
+    return *this;
+  }
+
+  [[gnu::always_inline]] [[nodiscard]] [[maybe_unused]]
+  constexpr auto Value() const noexcept -> T {
+    return this->value_;
+  }
+
+  [[gnu::always_inline]] [[nodiscard]] [[maybe_unused]]
+  constexpr auto PreviousValue() const noexcept -> T {
+    return this->previous_value_;
+  }
+
+  [[gnu::always_inline]] [[nodiscard]] [[maybe_unused]]
+  constexpr auto Delta() const noexcept -> T {
+    return this->value_ - this->previous_value_;
+  }
+
+private:
+  T value_;
+  T previous_value_;
+};
+
+using namespace units::literals;
+
+/**
+ * @brief Incremental conductance MPPT algorithm.
+ * @cite https://ww1.microchip.com/downloads/en/AppNotes/00001521A.pdf
+ * @param v_panel The current panel voltage.
+ * @param i_panel The current panel current.
+ * @returns An offset to be applied to the current duty cycle.
+ */
+auto MPPTIC(units::voltage::millivolt_u32_t v_panel, units::current::milliampere_u32_t i_panel) -> int32_t {
+  constexpr int32_t duty_step_size = 20; // How much to change the duty cycle per function call, range 0 - 1,000,000
+  static DeltaVariable<units::voltage::millivolt_u32_t> panel_voltage{v_panel}; // Keeps track of the change in panel voltage per function call
+  static DeltaVariable<units::current::milliampere_u32_t> panel_current{i_panel}; // Keeps track of the change in panel current per function call
+  static DeltaVariable<units::power::milliwatt_u32_t> panel_power{v_panel * i_panel}; // Keeps track of the change in panel power per function call
+
+  panel_voltage = v_panel; // Update the panel values
+  panel_current = i_panel;
+  panel_power = v_panel * i_panel;
+
+  if (panel_voltage.Delta().value() != 0) {
+    auto ratio = panel_power.Delta() / panel_voltage.Delta();
+    if (ratio.value() > 0) return duty_step_size;
+    else if (ratio.value() < 0) return -duty_step_size;
+  } else {
+    if (panel_current.Delta().value() > 0) return duty_step_size;
+    else if (panel_current.Delta().value() < 0) return -duty_step_size;
+  }
+  return 0; // Case when no change is required
+}
+
 auto pwm_01 = pwm::PWMManager<0, 1>(true);
 
 inline auto UARTPrint(std::string_view str) -> void {
-  for (auto letter : str) putc(letter, stdout);
+  for (auto letter: str) putc(letter, stdout);
 }
 
 inline auto UARTVPrint(fmt::string_view format_str, fmt::format_args args) -> void {
@@ -164,9 +83,9 @@ inline auto UARTVPrint(fmt::string_view format_str, fmt::format_args args) -> vo
   UARTPrint({buffer.data(), buffer.size()});
 }
 
-template <typename... T>
-inline auto FMTDebug(fmt::format_string<T...> fmt, T&&... args) -> void {
-  const auto& vargs = fmt::make_format_args(args...);
+template<typename... T>
+inline auto FMTDebug(fmt::format_string<T...> fmt, T &&... args) -> void {
+  const auto &vargs = fmt::make_format_args(args...);
   UARTVPrint(fmt, vargs);
 }
 
@@ -181,12 +100,16 @@ int32_t out_mW = 0;
 uint8_t run_state = 0;
 uint8_t duty = 1;
 
-auto PrintData(repeating_timer_t* rt) -> bool {
-  FMTDebug("VPV={}, IPV={}, PPV={}, VBAT={}, IBAT={}, PBAT={}, QBAT={}, SOC={}, RS={}, D={}\n", static_cast<float>(in_mV) / 1000.0F, static_cast<float>(in_mA) / 1000.0F, static_cast<float>(in_mW) / 1000.0F, static_cast<float>(out_mV) / 1000.0F, static_cast<float>(out_mA) / 1000.0F, static_cast<float>(out_mW) / 1000.0F, random_float(), random_float(), run_state, duty);
+auto PrintData(repeating_timer_t *rt) -> bool {
+  FMTDebug("VPV={}, IPV={}, PPV={}, VBAT={}, IBAT={}, PBAT={}, QBAT={}, SOC={}, RS={}, D={}\n",
+           static_cast<float>(in_mV) / 1000.0F, static_cast<float>(in_mA) / 1000.0F,
+           static_cast<float>(in_mW) / 1000.0F, static_cast<float>(out_mV) / 1000.0F,
+           static_cast<float>(out_mA) / 1000.0F, static_cast<float>(out_mW) / 1000.0F, random_float(), random_float(),
+           run_state, duty);
   return true;
 }
 
-auto UpdateADC(repeating_timer_t* rt) -> bool {
+auto UpdateADC(repeating_timer_t *rt) -> bool {
   constexpr int32_t voltage_gain = 13; // Voltage divider ratio
   constexpr int32_t current_offset = 1'024; // mV
   auto adc_26_mV = static_cast<int32_t>(rpp::adc::adc_26.ReadmV()); // Read all the ADC channels
@@ -197,7 +120,8 @@ auto UpdateADC(repeating_timer_t* rt) -> bool {
   in_mA = (adc_27_mV - current_offset) * 5 / 3; // This nice ratio is the result of simplifying the following:
   out_mV = adc_28_mV * voltage_gain;
   out_mA = (adc_29_mV - current_offset) * 5 / 3; // Gain 200 V/V * 1 / 6mΩ shunt * 0.5 divider ratio
-  in_mW = in_mV * in_mA / 1'000; // Then calculate power, dividing by 1,000 to remove the scaling ratio, keeping it in milliwatts
+  in_mW = in_mV * in_mA /
+          1'000; // Then calculate power, dividing by 1,000 to remove the scaling ratio, keeping it in milliwatts
   out_mW = out_mV * out_mA / 1'000;
   return true;
 }
@@ -211,10 +135,10 @@ auto main() -> int {
 
 //  for (int i = 0; i < 10; ++i) {
 //  while (true) {
-    gpio_put(led_pin, true);
-    sleep_ms(1000);
-    gpio_put(led_pin, false);
-    sleep_ms(1000);
+  gpio_put(led_pin, true);
+  sleep_ms(1000);
+  gpio_put(led_pin, false);
+  sleep_ms(1000);
 //  }
 
   rpp::adc::adc_26.Init().ReferenceVoltage(3300);  // Configure all the ADC pins
@@ -223,11 +147,13 @@ auto main() -> int {
   rpp::adc::adc_29.Init().ReferenceVoltage(3300);
 
   repeating_timer_t timer_print;                                       // Timer used to manage the feedback loop callback
-  if (!add_repeating_timer_ms(100, PrintData, nullptr, &timer_print))  // Create a timer which calls the print function at the specified rate
+  if (!add_repeating_timer_ms(100, PrintData, nullptr,
+                              &timer_print))  // Create a timer which calls the print function at the specified rate
     FMTDebug("Failed to create feedback loop timer\n");
 
   repeating_timer_t timer_adc;                                      // Timer used to manage the feedback loop callback
-  if (!add_repeating_timer_ms(10, UpdateADC, nullptr, &timer_adc))  // Create a timer which calls the print function at the specified rate
+  if (!add_repeating_timer_ms(10, UpdateADC, nullptr,
+                              &timer_adc))  // Create a timer which calls the print function at the specified rate
     FMTDebug("Failed to create feedback loop timer\n");
 
   srand(64);
@@ -237,28 +163,33 @@ auto main() -> int {
   while (true) {
     auto in_str = GetLine(buf, '\r');                   // Read in some serial data if available
     if (!in_str.empty()) {                              // If it is available, it will be in the string_view that sits within buf
-      std::string_view numbers = {&buf[1], buf.end()};  // Grab everything after the first character as a single string which contains a number
+      std::string_view numbers = {&buf[1],
+                                  buf.end()};  // Grab everything after the first character as a single string which contains a number
       int argument;                                     // Variable which the number sent over serial will be read into
 
-      auto [ptr, error] = std::from_chars(numbers.data(), numbers.end(), argument);  // Attempt to extract a number from the string
+      auto [ptr, error] = std::from_chars(numbers.data(), numbers.end(),
+                                          argument);  // Attempt to extract a number from the string
 
-      if (error == std::errc()) [[likely]]  // If there were no issues
-        FMTDebug("Attempting to change {} to {}\n", buf[0], argument);
-      else [[unlikely]]  // There was some problem with parsing the number
-        FMTDebug("Invalid argument, trouble parsing number\n");
+      if (error == std::errc())
+        [[likely]]  // If there were no issues
+              FMTDebug("Attempting to change {} to {}\n", buf[0], argument);
+      else
+        [[unlikely]]  // There was some problem with parsing the number
+              FMTDebug("Invalid argument, trouble parsing number\n");
 
       switch (buf[0]) {                                                                 // Decided what to do with the number based on the first character given
         case ('S'): {                                                                   // Change run state
-          run_state = (argument == std::clamp(argument, 0, 3)) ? argument : run_state;  // If the argument is within the valid range, change the run state to the argument, otherwise do nothing
+          run_state = (argument == std::clamp(argument, 0, 3)) ? argument
+                                                               : run_state;  // If the argument is within the valid range, change the run state to the argument, otherwise do nothing
           if (run_state >= 1) {
             pwm_01.Enable();
             pwm_01.DutyCycle(duty * 10'000);
-          }
-          else pwm_01.Disable();
+          } else pwm_01.Disable();
           break;
         }
         case ('D'): {                                                          // Change duty cycle
-          duty = (argument == std::clamp(argument, 1, 99)) ? argument : duty;  // If the argument is within the valid range, change the duty to the argument, otherwise do nothing
+          duty = (argument == std::clamp(argument, 1, 99)) ? argument
+                                                           : duty;  // If the argument is within the valid range, change the duty to the argument, otherwise do nothing
           pwm_01.DutyCycle(duty * 10'000);
           break;
         }
